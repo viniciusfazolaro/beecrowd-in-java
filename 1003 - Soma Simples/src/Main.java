ab54847dbab7cbfd55ef6e59738a6f5eedc44afb
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2023-08-09 02:35:18 -0300

    1003 - Soma Simples

--- 1003 - Soma Simples/src/Main.java
+++ 1003 - Soma Simples/src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
         
         // variable declaration
 
-        
+        int A, B, sum;
+
+        // data input
+
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+
+        // processing
+
+        sum = A + B;
+
+        // data output
+
+        System.out.println("SOMA = " + sum);
     }
 }
