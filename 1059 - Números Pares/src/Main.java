15f21605fa8e8a1eb61499398fdcbbe9e947e392
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2023-08-13 05:13:16 -0300

    1059 - Números Pares

---
+++ 1059 - Números Pares/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // variables declaration
+
+        int i;
+
+        // processing and output
+        for(i = 2; i <= 100; i += 2){
+            System.out.println("" + i);
+        }
+    }
+}
