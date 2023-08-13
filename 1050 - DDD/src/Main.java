6c8bbd0149534f92fcd4ee6bae19d7dfeb94bb0f
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2023-08-13 04:59:24 -0300

    1050 - DDD

---
+++ 1050 - DDD/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        // variables declaration
+
+        int ddd;
+
+        // input
+
+        ddd = scanner.nextInt();
+
+        // processing and output
+
+        switch(ddd){
+        case 61:
+            System.out.println("Brasilia");
+            break;
+        case 71:
+            System.out.println("Salvador");
+            break;
+        case 11:
+            System.out.println("Sao Paulo");
+            break;
+        case 21:
+            System.out.println("Rio de Janeiro");
+            break;
+        case 32:
+            System.out.println("Juiz de Fora");
+            break;
+        case 19:
+            System.out.println("Campinas");
+            break;
+        case 27:
+            System.out.println("Vitoria");
+            break;
+        case 31:
+            System.out.println("Belo Horizonte");
+            break;
+        default:
+            System.out.println("DDD nao cadastrado");
+            break;
+        }
+    }
+}
